e3e654f95ce083ed222302892bf1d80669c4624c
morteza Ebrahimi <morteza.nsrn@gmail.com>
2025-01-19 13:28:40 +0330

    Add TrendingCollection component and integrate data; enhance HeroSection layout

---
+++ nft-marketplace/src/data/data.ts
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    title: "DSGN Animals",
+    author: "MrFox",
+    likes: "1025+",
+    largeImage: "/src/assets/Animals/primary-photo-placeholder-1.png",
+    firstsmall: "/src/assets/Animals/dsgn-animal-small1.png",
+    secondesmall: "/src/assets/Animals/dsgn-animal-small2.png",
+    avatarIcon: "/src/assets/Animals/Avatar-Placeholder.png",
+    likeIcon: "/src/assets/Animals/Number-of-additional-NFTs.png"
+  },
+  {
+    title: "DSGN Animals",
+    author: "MrFox",
+    likes: "1025+",
+    largeImage: "path/to/dsgn-animals.png",
+    smallImages: [
+      "/images/dsgn-animal-small1.png",
+      "/images/dsgn-animal-small2.png",
+    ],
+  },
+  {
+    title: "DSGN Animals",
+    author: "MrFox",
+    likes: "1025+",
+    largeImage: "path/to/dsgn-animals.png",
+    smallImages: [
+      "/images/dsgn-animal-small1.png",
+      "/images/dsgn-animal-small2.png",
+    ],
+  },
+];
--- nft-marketplace/src/pages/homePage.tsx
+++ nft-marketplace/src/pages/homePage.tsx
@@ -1,11 +1,12 @@
 import gif from "../assets/HeroSection/heroanimationtransparentbck-2.gif";
 import { Button } from "../components/Button";
 import { Rocket } from "lucide-react";
-
+import { data } from "../data/data";
 export function HomePage() {
   return (
     <>
       <HeroSection />
+      <TrendingCollection />
     </>
   );
 }
@@ -26,10 +27,13 @@ function HeroSection() {
         <div className="py-4 md:hidden">
           <img src={gif} alt="hero-section" loading="lazy" />
         </div>
-          <Button variant="HeaderButton" className="md:py-3 md:px-6 md:w-2/3 md:h-12 my-3 md:rounded-2xl justify-self-center xl:w-1/2 xl:h-1 xl:px-5 xl:py-4 xl:rounded-xl" >
-            <Rocket  className="xl:w-3" />
-            <h2 className="md:text-xs xl:text-[10px]">Get started</h2>
-          </Button>
+        <Button
+          variant="HeaderButton"
+          className="md:py-3 md:px-6 md:w-2/3 md:h-12 my-3 md:rounded-2xl justify-self-center xl:w-1/2 xl:h-1 xl:px-5 xl:py-4 xl:rounded-xl"
+        >
+          <Rocket className="xl:w-3" />
+          <h2 className="md:text-xs xl:text-[10px]">Get started</h2>
+        </Button>
         <div className="flex flex-row gap-5 justify-center mt-4">
           <div className="text-center">
             <h2 className="font-semibold text-lg	">240K+</h2>
@@ -47,6 +51,48 @@ function HeroSection() {
       </div>
       <div className="py-4 hidden md:flex md:w-1/2">
         <img src={gif} alt="hero-section" loading="lazy" />
+      </div>
+    </section>
+  );
+}
+
+function TrendingCollection() {
+  return (
+    <section className="container mx-auto px-6 py-6 max-w-sm">
+      <div className="flex flex-col gap-2 py-5">
+        <h2 className="text-2xl font-bold">Trending Collection</h2>
+        <p className="text-sm">
+          Checkout our weekly updated trending collection.
+        </p>
+      </div>
+      <div className="container">
+        <div className="">
+          <img src={data[0].largeImage} alt="test" />
+        </div>
+        <div className="flex flex-row py-3 justify-around">
+          <img
+            src={data[0].firstsmall}
+            alt="firstSmallPhoto"
+            className="w-[5.5rem] h-[5.5rem]"
+          />
+          <img
+            src={data[0].secondesmall}
+            alt="secondeSmallPhoto"
+            className="w-[5.5rem] h-[5.5rem]"
+          />
+          <img
+            src={data[0].likeIcon}
+            alt="likeIcon"
+            className="w-[5.5rem] h-[5.5rem]"
+          />
+        </div>
+        <div className="flex flex-col gap-2 px-4 py-3">
+          <h2 className="text-xl font-semibold">{data[0].title}</h2>
+          <div className="name&logo flex align-center gap-2">
+            <img src={data[0].avatarIcon} alt="logo" className="size-6" />
+            <p>{data[0].author}</p>
+          </div>
+        </div>
       </div>
     </section>
   );
