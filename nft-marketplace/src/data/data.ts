de6ee97e0d01a620d5811e8f3cdceb6c45e6d42f
morteza Ebrahimi <morteza.nsrn@gmail.com>
2025-01-20 13:25:31 +0330

    Add new NFT collections and update asset paths; include Avatar placeholders for each collection

--- nft-marketplace/src/data/data.ts
+++ nft-marketplace/src/data/data.ts
@@ -1,32 +1,38 @@
 export const data = [
   {
-    title: "DSGN Animals",
-    author: "MrFox",
-    likes: "1025+",
-    largeImage: "/src/assets/Animals/primary-photo-placeholder-1.png",
-    firstsmall: "/src/assets/Animals/dsgn-animal-small1.png",
-    secondesmall: "/src/assets/Animals/dsgn-animal-small2.png",
-    avatarIcon: "/src/assets/Animals/Avatar-Placeholder.png",
-    likeIcon: "/src/assets/Animals/Number-of-additional-NFTs.png"
+    "title": "DSGN Animals",
+    "author": "MrFox",
+    "likes": "1025+",
+    "largeImage": "/src/assets/tranding-collectoin/Animals/primary-photo-placeholder-1.png",
+    "smallImages": [
+      "/src/assets/tranding-collectoin/Animals/dsgn-animal-small1.png",
+      "/src/assets/tranding-collectoin/Animals/dsgn-animal-small2.png",
+      "/src/assets/tranding-collectoin/Animals/Number-of-additional-NFTs.png"
+    ],
+    "avatarIcon": "/src/assets/tranding-collectoin/Animals/Avatar-Placeholder.png"
   },
   {
-    title: "DSGN Animals",
-    author: "MrFox",
-    likes: "1025+",
-    largeImage: "path/to/dsgn-animals.png",
-    smallImages: [
-      "/images/dsgn-animal-small1.png",
-      "/images/dsgn-animal-small2.png",
+    "title": "Magic Mushrooms",
+    "author": "Shroomie",
+    "likes": "532+",
+    "largeImage": "/src/assets/tranding-collectoin/Magic-Mushrooms/Primary-Photo-Placeholder-(1).png",
+    "smallImages": [
+      "/src/assets/tranding-collectoin/Magic-Mushrooms/Secondary-Photo-Placeholder-(1).png",
+      "/src/assets/tranding-collectoin/Magic-Mushrooms/Secondary-Photo-Placeholder.png",
+      "/src/assets/tranding-collectoin/Magic-Mushrooms/Number-of-additional-NFTs.png"
     ],
+    "avatarIcon": "/src/assets/tranding-collectoin/Magic-Mushrooms/Avatar-Placeholde.png"
   },
   {
-    title: "DSGN Animals",
-    author: "MrFox",
-    likes: "1025+",
-    largeImage: "path/to/dsgn-animals.png",
-    smallImages: [
-      "/images/dsgn-animal-small1.png",
-      "/images/dsgn-animal-small2.png",
+    "title": "Disco Machines",
+    "author": "BeKind2Robots",
+    "likes": "965+",
+    "largeImage": "/src/assets/tranding-collectoin/Disco-Machines/Primary-Photo-Placeholder-(1).png",
+    "smallImages": [
+      "/src/assets/tranding-collectoin/Disco-Machines/SecondaryPhotoPlaceholder.png",
+      "/src/assets/tranding-collectoin/Disco-Machines/SecondaryPhotoPlaceholder(1).png",
+      "/src/assets/tranding-collectoin/Disco-Machines/Number-of-additional-NFTs.png"
     ],
-  },
-];
+    "avatarIcon": "/src/assets/tranding-collectoin/Disco-Machines/Avatar-Placeholder.png"
+  }
+]
--- nft-marketplace/src/pages/homePage.tsx
+++ nft-marketplace/src/pages/homePage.tsx
@@ -6,7 +6,9 @@ export function HomePage() {
   return (
     <>
       <HeroSection />
-      <TrendingCollection />
+      <section>
+        <TrendingCollection />
+      </section>
     </>
   );
 }
@@ -57,43 +59,123 @@ function HeroSection() {
 }
 
 function TrendingCollection() {
+  const trendingItemOne = data[0];
+  const trendingItemTwo = data[1];
+  const trendingItemThree = data[2];
+
+  {console.log(trendingItemTwo.smallImages[0])}
+  {console.log(trendingItemOne.smallImages[0])}
+
   return (
-    <section className="container mx-auto px-6 py-6 max-w-sm">
-      <div className="flex flex-col gap-2 py-5">
-        <h2 className="text-2xl font-bold">Trending Collection</h2>
-        <p className="text-sm">
+    <section className="container  mx-auto px-6 py-6 max-w-sm md:max-w-3xl xl:max-w-5xl">
+      <div className="flex flex-col gap-2 py-5 md:py-10 xl:py-20">
+        <h2 className="text-2xl md:text-3xl xl:text-5xl font-bold">Trending Collection</h2>
+        <p className="text-sm md:text-base">
           Checkout our weekly updated trending collection.
         </p>
       </div>
-      <div className="container">
-        <div className="">
-          <img src={data[0].largeImage} alt="test" />
+
+    <section className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">    
+    <div className="container py-5 xl:py-1">
+        <div>
+          <img src={trendingItemOne.largeImage} alt="largeImage" />
         </div>
         <div className="flex flex-row py-3 justify-around">
-          <img
-            src={data[0].firstsmall}
-            alt="firstSmallPhoto"
-            className="w-[5.5rem] h-[5.5rem]"
-          />
-          <img
-            src={data[0].secondesmall}
-            alt="secondeSmallPhoto"
-            className="w-[5.5rem] h-[5.5rem]"
-          />
-          <img
-            src={data[0].likeIcon}
-            alt="likeIcon"
-            className="w-[5.5rem] h-[5.5rem]"
-          />
+          {trendingItemOne.smallImages.map((image, index) => (
+            <img
+              key={index}
+              className="w-[5.5rem] h-[5.5rem]"
+              src={image}
+              alt={`Trending Collection ${index + 1}`}
+            />
+          ))}
         </div>
         <div className="flex flex-col gap-2 px-4 py-3">
-          <h2 className="text-xl font-semibold">{data[0].title}</h2>
+          <h2 className="text-xl font-semibold">{trendingItemOne.title}</h2>
           <div className="name&logo flex align-center gap-2">
-            <img src={data[0].avatarIcon} alt="logo" className="size-6" />
-            <p>{data[0].author}</p>
+            <img
+              src={trendingItemOne.avatarIcon}
+              alt="logo"
+              className="size-6"
+            />
+            <p>{trendingItemOne.author}</p>
           </div>
         </div>
       </div>
+
+      <div className="container py-5 xl:py-1">
+        <div>
+          <img src={trendingItemTwo.largeImage} alt="largeImage" />
+        </div>
+        <div className="flex flex-row py-3 justify-around">
+          {trendingItemTwo.smallImages.map((image, index) => (
+            <img
+              key={index}
+              className="w-[5.5rem] h-[5.5rem]"
+              src={image}
+              alt={`Trending Collection ${index + 1}`}
+            />
+          ))}{" "}
+        </div>
+        <div className="flex flex-col gap-2 px-4 py-3">
+          <h2 className="text-xl font-semibold">{trendingItemTwo.title}</h2>
+          <div className="name&logo flex align-center gap-2">
+            <img
+              src={trendingItemTwo.avatarIcon}
+              alt="logo"
+              className="size-6"
+            />
+            <p>{trendingItemTwo.author}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container py-5 xl:py-1">
+        <div className="">
+          <img src={trendingItemThree.largeImage} alt="test" />
+        </div>
+        <div className="flex flex-row py-3 justify-around">
+          {trendingItemThree.smallImages.map((image, index) => (
+            <img
+              key={index}
+              className="w-[5.5rem] h-[5.5rem]"
+              src={image}
+              alt={`Trending Collection ${index + 1}`}
+            />
+          ))}{" "}
+        </div>
+        <div className="flex flex-col gap-2 px-4 py-3">
+          <h2 className="text-xl font-semibold">{trendingItemThree.title}</h2>
+          <div className="name&logo flex align-center gap-2">
+            <img src={trendingItemThree.avatarIcon} alt="logo" className="size-6" />
+            <p>{trendingItemThree.author}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container py-5 xl:py-1 hidden md:block xl:hidden">
+        <div className="">
+          <img src={trendingItemThree.largeImage} alt="test" />
+        </div>
+        <div className="flex flex-row py-3 justify-around">
+          {trendingItemThree.smallImages.map((image, index) => (
+            <img
+              key={index}
+              className="w-[5.5rem] h-[5.5rem]"
+              src={image}
+              alt={`Trending Collection ${index + 1}`}
+            />
+          ))}{" "}
+        </div>
+        <div className="flex flex-col gap-2 px-4 py-3">
+          <h2 className="text-xl font-semibold">{trendingItemThree.title}</h2>
+          <div className="name&logo flex align-center gap-2">
+            <img src={trendingItemThree.avatarIcon} alt="logo" className="size-6" />
+            <p>{trendingItemThree.author}</p>
+          </div>
+        </div>
+      </div>
+    </section>
     </section>
   );
 }
